782abd30f8119c5e85b5de5de0204746f59b294b
Kushtrim Bilali <kushtro20@gmail.com>
2025-07-14 20:54:13 +0200

    products site changes

--- frontend/app/components/Nav.jsx
+++ frontend/app/components/Nav.jsx
@@ -1,12 +1,18 @@
 "use client";
 import React from 'react'
 
-function page() {
+function Navigation() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/products">Products</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+  
 }
 
-export default page
+export default Navigation
