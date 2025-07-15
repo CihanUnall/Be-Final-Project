69458db2033121b9cb51631ad4354b7e74ad9201
Maliheh <malihe.kebriaei@dci-student.org>
2025-07-15 14:50:57 +0200

    bearbeitung frontend-BE

--- frontend/app/register/page.jsx
+++ frontend/app/register/page.jsx
@@ -14,7 +14,7 @@ export default function RegisterPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/register", {
+      const res = await fetch("http://localhost:3000/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
