7b0a332450f1ea0d2e33dd0a5d791ee588b2712e
RandyBorn <randyborn677@yahoo.com>
2025-07-15 17:36:39 +0200

    products

--- backend/db/mongoose.connect.js
+++ backend/db/mongoose.connect.js
@@ -2,26 +2,27 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const uri = process.env.MONGODB_URI;
-
   if (!uri || uri === "ENTER YOUR CONNECTION STRING HERE") {
-    console.error("❌ No valid MongoDB connection string found in .env!");
-    console.warn("👉 Look at the DB_MONGO_URI in .env-file.");
+    console.error(":x: No valid MongoDB connection string found in .env!");
+    console.warn(":nach_rechts_zeigen: Look at the DB_MONGO_URI in .env-file.");
     return;
   }
 
   try {
-    await mongoose.connect(uri);
-    console.log("✅ MongoDB connection established");
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 60000,
+    });
+    console.log(":weißes_häkchen: MongoDB connection established");
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
+    console.error(":x: MongoDB connection error:", err.message);
     process.exit(1);
   }
-
   mongoose.connection.on("error", (err) => {
-    console.error("❗ Mongoose runtime error:", err);
+    console.error(":ausrufezeichen: Mongoose runtime error:", err);
   });
-
   mongoose.connection.on("disconnected", () => {
-    console.warn("🔌 Mongoose disconnected");
+    console.warn(":stecker: Mongoose disconnected");
   });
 };
